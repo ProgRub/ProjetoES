663db0134cd0dcb4d3af08ac9ffcd977da364d65
Ruben Rodrigues <ruben2000@gmail.com>
2021-06-04 15:46:38 +0100

    Exercise has body part repository

--- PrescriptionSystem/ComponentsLibrary/Repositories/Implementations/ExerciseRepository.cs
+++ PrescriptionSystem/ComponentsLibrary/Repositories/Implementations/ExerciseRepository.cs
@@ -5,8 +5,10 @@ namespace ComponentsLibrary.Repositories.Implementations
 {
     public class ExerciseRepository:BaseRepository<Exercise>, IExerciseRepository
     {
+        private ExerciseHasBodyPartRepository _exerciseHasBodyPartRepository;
         public ExerciseRepository(PrescriptionSystemDbContext context) : base(context)
         {
+            _exerciseHasBodyPartRepository = new ExerciseHasBodyPartRepository(context);
         }
     }
 }
