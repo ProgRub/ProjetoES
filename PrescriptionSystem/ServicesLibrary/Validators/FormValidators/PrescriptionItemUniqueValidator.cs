969b4ff446836e6f81286b9a2ecc002375a43005
Ruben Rodrigues <ruben2000@gmail.com>
2021-06-22 10:12:00 +0100

    Refactoring PrescriptionItemUniqueValidator

--- PrescriptionSystem/ServicesLibrary/Validators/FormValidators/PrescriptionItemUniqueValidator.cs
+++ PrescriptionSystem/ServicesLibrary/Validators/FormValidators/PrescriptionItemUniqueValidator.cs
@@ -19,31 +19,21 @@ namespace ServicesLibrary.Validators.FormValidators
             {
                 case MedicineDTO medicine:
                 {
-                    foreach (var medicineInDB in PrescriptionItemService.Instance.GetAllMedicine())
-                    {
-                        if (medicineInDB.Name == medicine.Name && medicineInDB.Price == medicine.Price) return false;
-                    }
-                    return true;
+                    return PrescriptionItemService.Instance.GetAllMedicine().All(medicineInDB =>
+                        medicineInDB.Name != medicine.Name || medicineInDB.Price != medicine.Price);
                 }
                 case ExerciseDTO exercise:
                 {
-                    foreach (var exerciseInDB in PrescriptionItemService.Instance.GetAllExercises())
-                    {
-                        if (exerciseInDB.Name == exercise.Name &&
-                            exerciseInDB.BodyParts.All(e=>exercise.BodyParts.Contains(e)) &&
-                            exerciseInDB.Duration == exercise.Duration) return false;
-                    }
-                    return true;
+                    return PrescriptionItemService.Instance.GetAllExercises().All(exerciseInDB =>
+                        exerciseInDB.Name != exercise.Name ||
+                        !exerciseInDB.BodyParts.All(e => exercise.BodyParts.Contains(e)) ||
+                        exerciseInDB.Duration != exercise.Duration);
                 }
                 case TreatmentDTO treatment:
                 {
-                    foreach (var treatmentInDB in PrescriptionItemService.Instance.GetAllTreatments())
-                    {
-                        if (treatmentInDB.Name == treatment.Name &&
-                            treatmentInDB.BodyPart == treatment.BodyPart &&
-                            treatmentInDB.Duration == treatment.Duration) return false;
-                    }
-                    return true;
+                    return PrescriptionItemService.Instance.GetAllTreatments().All(treatmentInDB =>
+                        treatmentInDB.Name != treatment.Name || treatmentInDB.BodyPart != treatment.BodyPart ||
+                        treatmentInDB.Duration != treatment.Duration);
                 }
                 default:
                     throw new NotSupportedException($"Invalid type {request.GetType()}!");
